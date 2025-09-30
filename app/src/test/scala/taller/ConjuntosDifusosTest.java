280eff497d55bd725a4e1f7983527909c2896c1e
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-09-30 17:04:08 -0500

    test file with its structure added

---
+++ app/src/test/scala/taller/ConjuntosDifusosTest.java
@@ -0,0 +1,24 @@
+package taller;
+
+import junit.framework.TestCase;
+
+public class ConjuntosDifusosTest extends TestCase {
+
+    public void testPertenece() {
+    }
+
+    public void testIgualdad() {
+    }
+
+    public void testUnion() {
+    }
+
+    public void testInterseccion() {
+    }
+
+    public void testInclusion() {
+    }
+
+    public void testComplemento() {
+    }
+}
