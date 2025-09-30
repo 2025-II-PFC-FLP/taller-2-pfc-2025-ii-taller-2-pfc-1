029681a799c3e90dda296034ee76f880c758991c
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-09-30 17:43:11 -0500

    testPertenece and RunWith annotation added

--- app/src/test/scala/taller/ConjuntosDifusosTest.scala
+++ app/src/test/scala/taller/ConjuntosDifusosTest.scala
@@ -1,11 +1,21 @@
 package taller
 
+import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatestplus.junit.JUnitRunner
 
+
+@RunWith(classOf[JUnitRunner])
 class ConjuntosDifusosTest extends AnyFunSuiteLike {
+  val conjuntosDifusos = new ConjuntosDifusos()
 
   test("testPertenece") {
-
+    val conjDif1 = conjuntosDifusos.grande(100, 10)
+    assert(conjDif1(1001) == 1)
+    assert(conjDif1(999) < 1)
+    assert(conjDif1(400) <0.5)
+    assert(conjDif1(-1) == 0)
+    assert(conjDif1(-1001) == 0)
   }
 
   test("testIgualdad") {
