cf6e2c503697835dda5edd3978f203926bfa454b
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-09-30 17:17:41 -0500

    correction test file with its structure added

---
+++ app/src/test/scala/taller/ConjuntosDifusosTest.scala
@@ -0,0 +1,31 @@
+package taller
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class ConjuntosDifusosTest extends AnyFunSuiteLike {
+
+  test("testPertenece") {
+
+  }
+
+  test("testIgualdad") {
+
+  }
+
+  test("testUnion") {
+
+  }
+
+  test("testInterseccion") {
+
+  }
+
+  test("testInclusion") {
+
+  }
+
+  test("testComplemento") {
+
+  }
+
+}
