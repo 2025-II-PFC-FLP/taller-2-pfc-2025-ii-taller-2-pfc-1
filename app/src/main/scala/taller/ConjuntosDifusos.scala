41dccf854e9289c09209817a8cfe1ed2f1668a9c
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-09-30 16:50:55 -0500

    correction of TAD representation

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -1,20 +1,29 @@
 package taller
 
-class ConjuntosDifusos extends ConjDifuso {
+class ConjuntosDifusos {
   type ConjDifuso = Int => Double
 
 
-  override def pertenece(elem: Int, s: taller.ConjDifuso): Double = ???
+ def pertenece(elem: Int, s:ConjDifuso): Double = {
+  s(elem)
+ }
 
-  override def grande(d: Int, e: Int): taller.ConjDifuso = ???
+ def grande(d: Int, e: Int): ConjDifuso = {
+  def evalLargeNumber(n: Int): Double ={
+   if(n <= 0) 0.0
+   else if (n>0 && n< 1000) Math.pow(n/(n+d),e)
+   else 1.0
+  }
+  evalLargeNumber
+ }
 
-  override def complemento(c: taller.ConjDifuso): taller.ConjDifuso = ???
+ def complemento(c: ConjDifuso): ConjDifuso = ???
 
-  override def union(cd1: taller.ConjDifuso, cd2: taller.ConjDifuso): taller.ConjDifuso = ???
+ def union(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = ???
 
-  override def interseccion(cd1: taller.ConjDifuso, cd2: taller.ConjDifuso): taller.ConjDifuso = ???
+ def interseccion(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = ???
 
-  override def inclusion(cd1: taller.ConjDifuso, cd2: taller.ConjDifuso): Boolean = ???
+ def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = ???
 
-  override def igualdad(cd1: taller.ConjDifuso, cd2: taller.ConjDifuso): Boolean = ???
+ def igualdad(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = ???
 }
