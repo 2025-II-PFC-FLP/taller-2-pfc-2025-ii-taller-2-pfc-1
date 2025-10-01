a2f564a7eabd62ec60a7b491e58aed77da43ebb4
Juan David Cifuentes Florez <juan-12_91@hotmail.com>
2025-10-01 17:45:10 -0500

    correction on grande function, an inclusion and igualdad implementation addes as its test

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -1,5 +1,7 @@
 package taller
 
+import scala.annotation.tailrec
+
 class ConjuntosDifusos {
   type ConjDifuso = Int => Double
 
@@ -11,19 +13,30 @@ class ConjuntosDifusos {
  def grande(d: Int, e: Int): ConjDifuso = {
   def evalLargeNumber(n: Int): Double ={
    if(n <= 0) 0.0
-   else if (n>0 && n<1000) Math.pow(n.toDouble/(n+d).toDouble,e)
-   else 1.0
+   else Math.pow(n.toDouble/(n+d).toDouble,e)
   }
   evalLargeNumber
  }
 
- def complemento(c: ConjDifuso): ConjDifuso = ???
+ def complemento(c: ConjDifuso): ConjDifuso = {
+   x => 1.0 - c(x)
+ }
 
  def union(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = ???
 
  def interseccion(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = ???
 
- def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = ???
+ def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
+  @tailrec
+  def evalConjuntos(acc : Int) : Boolean = {
+   if(acc > 1000) true
+   else if (this.pertenece(acc, cd1) <= this.pertenece(acc, cd2)) evalConjuntos(acc + 1)
+   else false
+  }
+  evalConjuntos(0)
+ }
 
- def igualdad(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = ???
+ def igualdad(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
+   inclusion(cd1, cd2) && inclusion(cd2, cd1)
+ }
 }
--- app/src/test/scala/taller/ConjuntosDifusosTest.scala
+++ app/src/test/scala/taller/ConjuntosDifusosTest.scala
@@ -11,25 +11,23 @@ class ConjuntosDifusosTest extends AnyFunSuiteLike {
 
   test("testPertenece") {
     val conjDif1 = conjuntosDifusos.grande(5, 2)
-    assert(conjDif1(1001) == 1)
-    assert(conjDif1(900) > 0.98)
-    assert(conjDif1(5) > 0.2)
-    assert(conjDif1(20) > 0.5)
-    assert(conjDif1(-1001) == 0)
+    assert(conjuntosDifusos.pertenece(1001, conjDif1) > 0.99)
+    assert(conjuntosDifusos.pertenece(900, conjDif1) > 0.98)
+    assert(conjuntosDifusos.pertenece(5, conjDif1) >= 0.25)
+    assert(conjuntosDifusos.pertenece(20, conjDif1) > 0.5)
+    assert(conjuntosDifusos.pertenece(-1001, conjDif1) == 0)
   }
 
   test("testComplemento") {
-/*    val conjDif1 = conjuntosDifusos.grande(5, 2)
-    assert(conjDif1(1001) == 0)
-    assert(conjDif1(900) < 0.98)
-    assert(conjDif1(5) < 0.2)
-    assert(conjDif1(20) < 0.5)
-    assert(conjDif1(-1001) == 1)*/
+    val conjDifComplemento1 = conjuntosDifusos.complemento(conjuntosDifusos.grande(5, 2))
+
+    assert(conjuntosDifusos.pertenece(1001, conjDifComplemento1) < 0.01)
+    assert(conjuntosDifusos.pertenece(900, conjDifComplemento1) < 0.98)
+    assert(conjuntosDifusos.pertenece(5, conjDifComplemento1) <= 0.75)
+    assert(conjuntosDifusos.pertenece(20, conjDifComplemento1) < 0.5)
+    assert(conjuntosDifusos.pertenece(-1001, conjDifComplemento1) == 1)
   }
 
-  test("testIgualdad") {
-
-  }
 
   test("testUnion") {
 
@@ -40,9 +38,34 @@ class ConjuntosDifusosTest extends AnyFunSuiteLike {
   }
 
   test("testInclusion") {
+    val cDif1 = conjuntosDifusos.grande(5,2)
+    val cDif2 = conjuntosDifusos.grande(5,2)
+    val cDif3 = conjuntosDifusos.grande(2,2)
+    val cDif4 = conjuntosDifusos.grande(0,1)
+    val cDif5 = conjuntosDifusos.grande(20,3)
+    val cDif6 = conjuntosDifusos.grande(150,10)
 
+    assert(conjuntosDifusos.inclusion(cDif1, cDif2))
+    assert(conjuntosDifusos.inclusion(cDif1, cDif3))
+    assert(conjuntosDifusos.inclusion(cDif1, cDif4))
+    assert(!conjuntosDifusos.inclusion(cDif4,cDif1))
+    assert(conjuntosDifusos.inclusion(cDif6,cDif5))
   }
 
+  test("testIgualdad") {
+    val cDif1 = conjuntosDifusos.grande(5,2)
+    val cDif2 = conjuntosDifusos.grande(5,2)
+    val cDif3 = conjuntosDifusos.grande(2,2)
+    val cDif4 = conjuntosDifusos.grande(0,1)
+    val cDif5 = conjuntosDifusos.grande(20,3)
+    val cDif6 = conjuntosDifusos.grande(20,3)
+
+    assert(conjuntosDifusos.igualdad(cDif1, cDif2))
+    assert(!conjuntosDifusos.igualdad(cDif1, cDif3))
+    assert(!conjuntosDifusos.igualdad(cDif1, cDif4))
+    assert(!conjuntosDifusos.igualdad(cDif4,cDif1))
+    assert(conjuntosDifusos.igualdad(cDif6,cDif5))
+  }
 
 
 }
